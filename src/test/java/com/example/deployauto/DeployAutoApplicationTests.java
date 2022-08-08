35164308e9f621202f26d5c63b6d6c2fe3d98783
ward <bonuswon@gmail.com>
2022-08-08 15:30:42 +0900

    1st commit

---
+++ src/test/java/com/example/deployauto/DeployAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
